6ce498ad494d3b00921778d3f0d247a28b4ded3d
tsora1603 <neiva.cesar07@proton.me>
2026-02-18 09:09:24 -0300

    Fix install script

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 dir=$(pwd)
 git pull
-if [ -d "$HOME/.local/share/icons/pixelitos-dark" ] || [ -d "$HOME/.local/share/icons/pixelitos-light" ]; then
-	echo 'Pixora Icon Theme already installed.'
+if [ -d "$HOME/.local/share/icons/pixelitos-dark" ] && [ -d "$HOME/.local/share/icons/pixelitos-light" ] && [ -d "$HOME/.local/share/icons/pixora" ]; then
+	echo 'pixora-theme already installed.'
 	exit 1
 fi
+
 if [ ! -d "$HOME/.local/share/icons" ]; then
 	mkdir "$HOME/.local/share/icons"
+fi
+if [ ! -d "$HOME/.local/share/icons/pixelitos-dark" ]; then
+	mkdir "$HOME/.local/share/icons/pixelitos-dark"
+fi
+if [ ! -d "$HOME/.local/share/icons/pixelitos-light" ]; then
+	mkdir "$HOME/.local/share/icons/pixelitos-light"
+fi
+if [ ! -d "$HOME/.local/share/icons/pixora" ]; then
+	mkdir "$HOME/.local/share/icons/pixora"
 fi
 
 echo 'How would you like to install? options:'
@@ -15,18 +25,21 @@ read -p 'option: ' choice
 if [ "$choice" == "1" ]; then
 	ln -s "$dir/pixelitos-dark" "$HOME/.local/share/icons/pixelitos-dark"
 	ln -s "$dir/pixelitos-light" "$HOME/.local/share/icons/pixelitos-light"
-elif [ "$choice" == "2" ]; then
-	cp -r "$dir/pixelitos-dark" "$HOME/.local/share/icons/pixelitos-dark"
-	cp -r "$dir/pixelitos-light" "$HOME/.local/share/icons/pixelitos-light"
-	echo "You are about to remove the directory $dir. Are you sure? (y/n)"
-	read confirm
-	if [ "$confirm" == "y" ]; then
-		rm -rf "$dir"
-	else
-		echo "Directory removal cancelled."
+	ln -s "$dir/pixora" "$HOME/.local/share/icons/pixora"
+	elif [ "$choice" == "2" ]; then
+		cp -r "$dir/pixelitos-dark" "$HOME/.local/share/icons/pixelitos-dark"
+		cp -r "$dir/pixelitos-light" "$HOME/.local/share/icons/pixelitos-light"
+		cp -r "$dir/pixora" "$HOME/.local/share/icons/pixora"
+		echo "You are about to remove the directory $dir. Are you sure? (y/n)"
+		read confirm
+		if [ "$confirm" == "y" ]; then
+			rm -rf "$dir"
+		else
+			echo "Directory removal cancelled."
 	fi
 else
 	echo 'Exiting as no valid option was selected.'
 	exit 1
 fi
+
 echo 'Thanks for installing the Pixora Icon Theme!'
